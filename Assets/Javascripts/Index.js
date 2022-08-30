cb0ad090c7dd1434be50a741225d0af2f6c5befb
abdullatif <ltofmhmd2004@gmail.com>
2022-08-31 01:20:26 +0300

    V0.2
    Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

---
+++ Assets/Javascripts/Index.js
@@ -0,0 +1,158 @@
+// Workin test
+function log() {
+let JsTest = ("Javascript is workin noice")
+console.log(JsTest);
+}
+// Workin test end
+
+
+// Side javas
+
+let btn = document.getElementById("btn")
+let sidebar = document.getElementById("sidebar")
+let searchobtn = document.getElementById("bx_search")
+
+function sidebtnclick() {
+ document.getElementById("sidebar").classList.toggle("active")
+
+ document.getElementById("logo-name").style.color = ("#11101d")
+ document.getElementById("nav-itemso").style.display = ("flex")
+
+ document.getElementById("home_content").style.left = ("78px")
+ document.getElementById("imgoo").style.marginRight = ("0px")
+ document.getElementById("mid-navo").style.left = ("78px")
+ 
+
+ document.getElementById("small_side0").style.display = ("flex")
+ document.getElementById("small_side").style.display = ("flex")
+ document.getElementById("small_side1").style.display = ("flex")
+ document.getElementById("small_side2").style.display = ("flex")
+ document.getElementById("small_side3").style.display = ("flex")
+ document.getElementById("small_side4").style.display = ("flex")
+ document.getElementById("small_side5").style.display = ("flex")
+ document.getElementById("small_side6").style.display = ("flex")
+ 
+ document.getElementById("links_name").style.display = ("none")
+ document.getElementById("links_name1").style.display = ("none")
+ document.getElementById("links_name2").style.display = ("none")
+ document.getElementById("links_name3").style.display = ("none")
+ document.getElementById("links_name4").style.display = ("none")
+ document.getElementById("links_name5").style.display = ("none")
+ document.getElementById("links_name6").style.display = ("none")
+ 
+ document.getElementById("name").style.display = ("none")
+ document.getElementById("job").style.display = ("none")
+ document.getElementById("log_out").style.display = ("none")
+
+ 
+if (document.getElementById("sidebar").classList.contains("active")) {
+
+ document.getElementById("logo-name").style.color = ("white")
+ document.getElementById("nav-itemso").style.display = ("none")
+
+ document.getElementById("home_content").style.left = ("240px")
+ document.getElementById("imgoo").style.marginRight = ("210px")
+ document.getElementById("mid-navo").style.left = ("240px")
+ 
+
+ document.getElementById("small_side0").style.display = ("none")
+ document.getElementById("small_side").style.display = ("none")
+ document.getElementById("small_side1").style.display = ("none")
+ document.getElementById("small_side2").style.display = ("none")
+ document.getElementById("small_side3").style.display = ("none")
+ document.getElementById("small_side4").style.display = ("none")
+ document.getElementById("small_side5").style.display = ("none")
+ document.getElementById("small_side6").style.display = ("none")
+   
+ document.getElementById("links_name").style.display = ("flex")
+ document.getElementById("links_name1").style.display = ("flex")
+ document.getElementById("links_name2").style.display = ("flex")
+ document.getElementById("links_name3").style.display = ("flex")
+ document.getElementById("links_name4").style.display = ("flex")
+ document.getElementById("links_name5").style.display = ("flex")
+ document.getElementById("links_name6").style.display = ("flex")
+   
+ document.getElementById("name").style.display = ("flex")
+ document.getElementById("job").style.display = ("flex")
+ document.getElementById("log_out").style.display = ("flex")
+}
+}
+
+
+
+// other function stoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooop
+
+function searchbtnclick() {
+ document.getElementById("sidebar").classList = ("sidebar active")
+   
+ document.getElementById("logo-name").style.color = ("#11101d")
+ document.getElementById("nav-itemso").style.display = ("flex")
+   
+ document.getElementById("home_content").style.left = ("78px")
+ document.getElementById("home_content").style.marginRight = ("0px")
+ document.getElementById("imgoo").style.marginRight = ("0px")
+ document.getElementById("mid-navo").style.left = ("78px")
+    
+   
+ document.getElementById("small_side0").style.display = ("flex")
+ document.getElementById("small_side").style.display = ("flex")
+ document.getElementById("small_side1").style.display = ("flex")
+ document.getElementById("small_side2").style.display = ("flex")
+ document.getElementById("small_side3").style.display = ("flex")
+ document.getElementById("small_side4").style.display = ("flex")
+ document.getElementById("small_side5").style.display = ("flex")
+ document.getElementById("small_side6").style.display = ("flex")
+    
+ document.getElementById("links_name").style.display = ("none")
+ document.getElementById("links_name1").style.display = ("none")
+ document.getElementById("links_name2").style.display = ("none")
+ document.getElementById("links_name3").style.display = ("none")
+ document.getElementById("links_name4").style.display = ("none")
+ document.getElementById("links_name5").style.display = ("none")
+ document.getElementById("links_name6").style.display = ("none")
+    
+ document.getElementById("name").style.display = ("none")
+ document.getElementById("job").style.display = ("none")
+ document.getElementById("log_out").style.display = ("none")
+   
+    
+if (document.getElementById("sidebar").classList.contains("active")) {
+   
+ document.getElementById("logo-name").style.color = ("white")
+ document.getElementById("nav-itemso").style.display = ("none")
+
+ document.getElementById("home_content").style.left = ("240px")
+ document.getElementById("home_content").style.marginRight = ("210px")
+ document.getElementById("imgoo").style.marginRight = ("210px")
+ document.getElementById("mid-navo").style.left = ("240px")
+    
+   
+ document.getElementById("small_side0").style.display = ("none")
+ document.getElementById("small_side").style.display = ("none")
+ document.getElementById("small_side1").style.display = ("none")
+ document.getElementById("small_side2").style.display = ("none")
+ document.getElementById("small_side3").style.display = ("none")
+ document.getElementById("small_side4").style.display = ("none")
+ document.getElementById("small_side5").style.display = ("none")
+ document.getElementById("small_side6").style.display = ("none")
+      
+ document.getElementById("links_name").style.display = ("flex")
+ document.getElementById("links_name1").style.display = ("flex")
+ document.getElementById("links_name2").style.display = ("flex")
+ document.getElementById("links_name3").style.display = ("flex")
+ document.getElementById("links_name4").style.display = ("flex")
+ document.getElementById("links_name5").style.display = ("flex")
+ document.getElementById("links_name6").style.display = ("flex")
+      
+ document.getElementById("name").style.display = ("flex")
+ document.getElementById("job").style.display = ("flex")
+ document.getElementById("log_out").style.display = ("flex")
+}
+}
+   
+   
+   
+
+
+
+
