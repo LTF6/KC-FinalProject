861e351dffeb37191acec1b0609578d05d2ae204
abdullatif <ltofmhmd2004@gmail.com>
2022-09-03 22:25:03 +0300

    V0.6

--- Assets/Javascripts/Subjective Tests.js
+++ Assets/Javascripts/Subjective Tests.js
@@ -159,4 +159,59 @@ if (document.getElementById("sidebar").classList.contains("active")) {
 
 }
 }
-   
+
+
+//  the real work
+function showques() {
+ document.getElementById("questions-form").style.display = ( "block" )
+}
+
+// next Function
+function Next1() {
+ document.getElementById("quests1").style.display = ("none")
+ document.getElementById("quests2").style.display = ("none")
+ document.getElementById("nxt-btno-hio").style.display = ("none")
+
+ document.getElementById("quests3").style.display = ("block")
+ document.getElementById("quests4").style.display = ("block")
+ document.getElementById("nxt-btno-hio2").style.display = ("block")
+}
+
+
+function Next2() {
+ document.getElementById("quests3").style.display = ("none")
+ document.getElementById("quests4").style.display = ("none")
+ document.getElementById("nxt-btno-hio2").style.display = ("none")
+
+ document.getElementById("quests5").style.display = ("block")
+ document.getElementById("quests6").style.display = ("block")
+ document.getElementById("nxt-btno-hio3").style.display = ("block")
+}
+
+
+function Next3() {
+ document.getElementById("quests5").style.display = ("none")
+ document.getElementById("quests6").style.display = ("none")
+ document.getElementById("nxt-btno-hio3").style.display = ("none")
+ 
+ document.getElementById("quests7").style.display = ("block")
+ document.getElementById("quests8").style.display = ("block")
+ document.getElementById("nxt-btno-hio4").style.display = ("block")
+}
+
+function Next4() {
+ document.getElementById("quests7").style.display = ("none")
+ document.getElementById("quests8").style.display = ("none")
+ document.getElementById("nxt-btno-hio4").style.display = ("none")
+ 
+ document.getElementById("quests9").style.display = ("block")
+ document.getElementById("contact-bar-tst").style.display = ("block")
+}
+
+function unshowques() {
+ document.getElementById("questions-form").style.display = ( "none" )
+}
+
+function consolelog() {
+    console.log("js is workin ?");
+}
